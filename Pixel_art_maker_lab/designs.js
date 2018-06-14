893f4d341f5bca081283ea3e6d89b052f03b1e6a
yassinejaoudi <jaoudiyassine@gmail.com>
2018-06-14 16:08:36 -0400

    Add JS event listeners using jQuery

--- Pixel_art_maker_lab/designs.js
+++ Pixel_art_maker_lab/designs.js
@@ -1,20 +1,49 @@
-// Select color input
-let colorInput = document.querySelector('#colorPicker').value;
-
-// Select size input
-let heightValue = document.querySelector('#inputHeight').value;
-let widthValue = document.querySelector('#inputWidth').value;
-
-// When size is submitted by the user, call makeGrid()
-const submitedValue = document.querySelector('#choiceSubmit');
-submitedValue.addEventListener('submit', (event) => {
-	makeGrid(event);
+$('#submitChoice').on('click', function(event){
+  event.preventDefault();
+  makeGrid();
 });
 
-function makeGrid(event) {
-  	for (var row = 0; row < widthValue; row++){
-      	$('#pixelCanvas').append('<tr></tr>');
-    }for(var column = 0; column < heightValue; column++){
-      	$('tr').append('<td></td>');
+function makeGrid(){
+  const table = $('#pixelCanvas');
+  table.empty();
+  let height = $('#inputHeight').val();
+  let width = $('#inputWidth').val();
+  
+  
+  for(var row=0; row < width; row++){
+        table.append('<tr></tr');
     }
+    for(var column=0; column < height; column++){
+        $('tr').append('<td></td>');
+    }
+  
+    // Set Color
+    $('#colorPicker').on('change', function(){
+        color = $('#colorPicker').val();
+        $(input).css('border-color', color);
+    });
+
+    //Mouse Click
+    $(table).on('click', 'td', function(evt){
+        evt.preventDefault();
+        $(this).css('background-color', color);
+    });
+
+    // Mouse Down
+    $(table).on('mousedown', function(){
+        mouseDown = true;
+    });
+
+    // Mouse Up
+    $(table).on('mouseup', function(){
+        mouseDown = false;
+    });
+
+    // Clicked Mouse Moving
+    $(table).on('mousemove', 'td', function(evt){
+        evt.preventDefault();
+        if (mouseDown){
+            $(this).css('background-color', color);
+        }
+    })
 }
